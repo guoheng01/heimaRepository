7a5a55076ce4d862dbcdff9e840448ea78aa287f
guoheng01 <guoheng45@163.com>
2020-08-19 16:13:04 +0800

    网络爬虫HttpClient添加链接池

--- itcast-crawler-first/src/main/java/cn/itcast/crawler/test/HttpGetParamTest.java
+++ itcast-crawler-first/src/main/java/cn/itcast/crawler/test/HttpGetParamTest.java
@@ -8,6 +8,7 @@ import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.utils.URIBuilder;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
+import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
 import org.apache.http.util.EntityUtils;
 
 import java.io.IOException;
@@ -19,24 +20,37 @@ import java.net.URISyntaxException;
 public class HttpGetParamTest {
 
     public static void main(String[] args) {
-        //1.打开浏览器:创建HTTPClient对象
-        CloseableHttpClient httpClient = HttpClients.createDefault();
+
+        //8.创建连接池管理器
+        PoolingHttpClientConnectionManager phccm = new PoolingHttpClientConnectionManager();
+            //8.1设置最大连接数
+            phccm.setMaxTotal(200);
+            //8.2设置每个服务主机最大并发数
+            phccm.setDefaultMaxPerRoute(20);
+        //9.使用连接池管理器发送请求
+            doGet(phccm);
+            doGet(phccm);
+    }
+    private static void doGet(PoolingHttpClientConnectionManager phccm){
+
+        //1.打开浏览器，从连接池管理器中获取HTTPClient对象
+        CloseableHttpClient httpClient = HttpClients.custom().setConnectionManager(phccm).build();
         URIBuilder uriBuilder = null;
         CloseableHttpResponse response = null;
         try {
             //https://www.baidu.com/baidu?tn=monline_3_dg&ie=utf-8&wd=java
             //https://www.runoob.com/?s=java
             //2.输入网址，创建urlBuilder对象
-            uriBuilder = new URIBuilder("https://www.runoob.com/");
+            uriBuilder = new URIBuilder("https://www.baidu.com/baidu");
             //3.封装请求参数
             uriBuilder.setParameter("s","java")
-//                    .setParameter("ie","utf-8")
-//                    .setParameter("wd","java")
+                    .setParameter("ie","utf-8")
+                    .setParameter("wd","java")
                     .build();
             //4.创建get对象
             HttpGet httpGet = new HttpGet(String.valueOf(uriBuilder));
             //5.使用客户端对象发起请求
-             response = httpClient.execute(httpGet);
+            response = httpClient.execute(httpGet);
             //6.解析返回对象
             if(response.getStatusLine().getStatusCode() == 200){
                 //获取响应体
@@ -53,11 +67,12 @@ public class HttpGetParamTest {
         } catch (IOException e) {
             e.printStackTrace();
         }finally {
-            //关闭请求连接
+            //7.关闭请求连接
             if(response == null){
                 try {
                     response.close();
-                    httpClient.close();
+                    //从连接池获取的连接不需要关闭
+                    // httpClient.close();
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
--- itcast-crawler-first/src/main/java/cn/itcast/crawler/test/HttpPostParamTest.java
+++ itcast-crawler-first/src/main/java/cn/itcast/crawler/test/HttpPostParamTest.java
@@ -8,6 +8,7 @@ import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
+import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
 import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.util.EntityUtils;
 
@@ -20,36 +21,58 @@ import java.util.ArrayList;
  */
 public class HttpPostParamTest {
     public static void main(String[] args) {
-        //1.打开浏览器
-        CloseableHttpClient httpClient = HttpClients.createDefault();
-        //2.输入请求地址
-        HttpPost httpPost = new HttpPost("https://www.baidu.com/baidu");
+        //7.创建连接池管理器
+        PoolingHttpClientConnectionManager phccm = new PoolingHttpClientConnectionManager();
+            //7.1设置最大连接数
+            phccm.setMaxTotal(200);
+            //7.2设置每个服务主机的最大并发数
+            phccm.setDefaultMaxPerRoute(20);
+        //8.使用连接池管理器发送请求
+        doPost(phccm);
+        doPost(phccm);
+    }
+    private static void doPost(PoolingHttpClientConnectionManager phccm){
+        //1.打开浏览器，从连接池管理器中获取HTTPClient对象
+        CloseableHttpClient httpClient = HttpClients.custom().setConnectionManager(phccm).build();
+        //2.输入请求地址 https://www.runoob.com/?s=java
+        HttpPost httpPost = new HttpPost("https://www.runoob.com/");
         //3.List集合封装表单请求参数
         ArrayList<NameValuePair> params = new ArrayList<>();
-        params.add(new BasicNameValuePair("tn","monline_3_dg"));
-        params.add(new BasicNameValuePair("ie","utf-8"));
-        params.add(new BasicNameValuePair("wd","java"));
+        params.add(new BasicNameValuePair("s","java"));
+//        params.add(new BasicNameValuePair("ie","utf-8"));
+//        params.add(new BasicNameValuePair("wd","java"));
         //4.创建表单的实体对象，将请求参数放到实体对象中
+        CloseableHttpResponse response = null;
         try {
             UrlEncodedFormEntity formEntity = new UrlEncodedFormEntity(params, "utf-8");
             //5.将参数实体对象放到httpPost请求对象中
             httpPost.setEntity(formEntity);
             //6.使用httpClient对象发起请求
-            CloseableHttpResponse response = httpClient.execute(httpPost);
-            //6.解析响应对象
-                //6.1判断响应状态码是否为200
-                if(response.getStatusLine().getStatusCode() == 200){
-                    //获取响应体
-                    HttpEntity entity = response.getEntity();
-                    //将响应体转换成字符串
-                    String content = EntityUtils.toString(entity, "utf-8");
-                    System.out.println(content);
-                }
+           response = httpClient.execute(httpPost);
+            //7.解析响应对象
+            //7.1判断响应状态码是否为200
+            if(response.getStatusLine().getStatusCode() == 200){
+                //7.2获取响应体
+                HttpEntity entity = response.getEntity();
+                //7.3将响应体转换成字符串
+                String content = EntityUtils.toString(entity, "utf-8");
+                System.out.println(content);
+            }
         } catch (UnsupportedEncodingException e) {
             e.printStackTrace();
         } catch (ClientProtocolException e) {
             e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
+        }finally {
+            //释放连接
+            try {
+                response.close();
+                //不能关闭HttpClient
+                //httpClient.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
-    }}
+    }
+}
